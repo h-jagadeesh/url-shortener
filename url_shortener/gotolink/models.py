ec046ad14af1557557a911383f0c0f5fee68ef3c
H Jagadeesh <h.jagadeesh.work@gmail.com>
2021-11-09 16:55:09 +0530

    Defined database elements

--- url_shortener/gotolink/models.py
+++ url_shortener/gotolink/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class urls(models.Model):
+	og_link = models.CharField(max_length=1000)
+	new_link = models.CharField(max_length=20)
+
+	def __str__(self):
+		return f"{self.id}:{self.og_link} shortened to {self.new_link}"
