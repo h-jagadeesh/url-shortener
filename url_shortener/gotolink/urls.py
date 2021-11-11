e024ff5a9e1cfc1e37b76d5277b9327c8188d58f
H Jagadeesh <h.jagadeesh.work@gmail.com>
2021-11-11 10:38:49 +0530

    .

--- url_shortener/gotolink/urls.py
+++ url_shortener/gotolink/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "gotolink"
 urlpatterns = [
 	path("",views.index,name="index"),
 	path("success",views.success,name="success"),
--- url_shortener/gotolink/views.py
+++ url_shortener/gotolink/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import urls
 # Create your views here.
 
 def index(request):
 	return render(request,"gotolink/index.html")
 
 def success(request):
+	#url = 
 	return render(request,"gotolink/success.html")
